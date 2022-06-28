46517edda190b2cde874671707de7901c5b406da
vladimir klonin <vklonin@gmail.com>
2022-06-28 16:28:43 +0400

    updated functions

--- homework.py
+++ homework.py
@@ -5,15 +5,26 @@
 
 def open_browser(browser_name):
     pass
+    print_function_description(open_browser, locals())
 
 
 def go_to_companyname_homepage(page_url):
     pass
+    print_function_description(go_to_companyname_homepage, locals())
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    print_function_description(find_registration_button_on_login_page("url","button_text"))
     pass
+    print_function_description(find_registration_button_on_login_page, locals())
 
-def print_function_description(func):
-    print(func.__name__)
+def print_function_description(func, locals):
+    list_of_words = func.__name__.replace("_", " ").split(" ")
+    list_of_words[0] = "Function: " + list_of_words[0].capitalize()
+    list_of_words[len(list_of_words)-1] = list_of_words[len(list_of_words)-1] + ". with follwing args: " +', '.join(list(locals.values()))
+    print(' '.join(list_of_words))
+
+
+
+find_registration_button_on_login_page("www.google.com", "Submit")
+go_to_companyname_homepage("www.cia.us")
+open_browser("chromium")
